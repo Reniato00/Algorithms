d3490d5ebc582dca6a78234cef75ca984fdf9880
Reniato00 <renyralo@hotmail.com>
2025-04-23 15:43:03 -0600

    Add TwoSumWithHashmap

---
+++ TwoSumWithHashMap.js
@@ -0,0 +1,51 @@
+// You have an array with any number of elements , Example [1,6,4,9]
+// and a target that is the sum of two values of the array , Example 15
+// return a value of the indices of the two values that add up to the target, Example [1,3]
+
+const array = [1,6,4,9];
+const target = 15;
+
+function findIndices(array, target) 
+{
+    const saved  = {};
+
+    for (let i = 0; i < array.length; i++) 
+    {
+        const current = array[i];
+
+        const difference = target - current;
+
+        if (saved[difference] !== undefined) 
+        {
+            return [saved[difference], i];
+        }
+        saved[current] = i;
+    }
+
+    return []
+}
+
+function SearchIndices(array, target)
+{
+    const saved = {};
+
+    array.forEach((element,index) => {
+        const currentValue = element;
+        const difference = target - currentValue;
+
+        if(saved[difference] !== undefined)
+        {
+            return [saved[difference], index]; //Thats return will be invoked when the difference is found in the Hasmap object 
+        }
+
+        saved[currentValue] = index;
+    });
+
+    return [];
+}
+
+const result = findIndices(array, target);
+const otherResult = SearchIndices(array, target);
+
+console.log(result); 
+console.log(otherResult);
